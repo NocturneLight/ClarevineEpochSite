8c1520ab1e8fc9d04c762d6272841e2396322d22
Antonio <55106060+NocturneLight@users.noreply.github.com>
2022-10-09 16:20:11 -0500

    -utilities.py added with a function to get the date and time. -Copyright info in layout.html adjusted to display year of origination and the current year.

--- ClarevineEpochSite/ClarevineEpochSite/__init__.py
+++ ClarevineEpochSite/ClarevineEpochSite/__init__.py
@@ -5,4 +5,8 @@ The flask application package.
 from flask import Flask
 app = Flask(__name__)
 
+# Adds utilities.py to the application package.
+from .backend.utilities import utilities
+app.register_blueprint(utilities)
+
 import ClarevineEpochSite.views
